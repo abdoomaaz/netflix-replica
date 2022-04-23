9e780b2271a1dd15042785d79a66835fe8a8d362
abdoomaaz <abodeh98@gmail.com>
2022-04-23 15:02:16 +0200

    Adds a tableview in the main screen and base project structure

--- Netflix/MainTabBarViewController.swift
+++ Netflix/MainTabBarViewController.swift
@@ -30,10 +30,10 @@ class MainTabBarViewController: UITabBarController {
         vc4.title = "Downloads"
         
         tabBar.tintColor = .label
-        
         setViewControllers([vc1, vc2, vc3,vc4], animated: true)
 
 
     }
+    
 }
 
