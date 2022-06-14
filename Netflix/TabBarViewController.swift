e933fa0ca4a7842b8d4bc2883250158b92af276d
abdoomaaz <abodeh98@gmail.com>
2022-06-14 18:18:33 +0200

    moves appdelegate and renames tabbar

--- Netflix/TabBarViewController.swift
+++ Netflix/TabBarViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainTabBarViewController: UITabBarController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
